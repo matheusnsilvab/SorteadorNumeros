a56289faecd6ad408e0c1f93a4f14fda67ea68a4
matheusnsilvab@gmail.com <matheusnsilvab@gmail.com>
2023-02-01 21:27:16 -0300

    criando um for para fazer um loop

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
 var numeroSorteado = document.querySelector("div.box h1");
 var botaoSortear = document.querySelector("div.box button");
 var roleta = []
+for(i = 0; 1<=50; i++) {
+   roleta.push(i)
+}
