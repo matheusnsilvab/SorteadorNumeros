c233fcc6bccbda8739599509abb7696477e60b45
matheusnsilvab@gmail.com <matheusnsilvab@gmail.com>
2023-02-01 21:36:56 -0300

    erro de digitação resolvido :heavy_check_mark:

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 var numeroSorteado = document.querySelector("div.box h1");
 var botaoSortear = document.querySelector("div.box button");
 var roleta = []
-for(i = 0; 1<=50; i++) {
+for(i = 0; i <=50; i++) {
    roleta.push(i)
 }
 
